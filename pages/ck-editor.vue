153c55ad63edccb8ff932d0b88f8f643abdeb6b2
pupuzel <jjdo1994@gmail.com>
2021-04-20 15:11:58 +0900

    ckeditor

--- pages/ck-editor.vue
+++ pages/ck-editor.vue
@@ -1,66 +1,73 @@
 <template>
     <div>
-       <ckeditor :editor="editor" :value="editorData" :config="editorConfig"/>
+        <ckeditor :editor="editor" :value="editorData" :config="editorConfig"/>
     </div>
 </template>
 
 <script>
-    import CKEditor from '@ckeditor/ckeditor5-vue2'
-    import CustomBuild from '~/modules/ckeditor5-build-custom/build/ckeditor'
-    
-    export default {
-        created(){
+let CKEditor
+let CustomBuild
 
-        },
-        components: {
-            ckeditor: CKEditor.component
-        },
-        data() {
-            return {
-                editor: CustomBuild,
-                editorData: '<p>Content of the editor.</p>',
-                editorConfig: {
-                        toolbar: {
-                            items: [
-                                'heading',
-                                '|',
-                                'fontColor',
-                                'fontBackgroundColor',
-                                'bold',
-                                'italic',
-                                'link',
-                                'bulletedList',
-                                'numberedList',
-                                '|',
-                                'outdent',
-                                'indent',
-                                '|',
-                                'imageUpload',
-                                'blockQuote',
-                                'insertTable',
-                                'mediaEmbed',
-                                'undo',
-                                'redo'
-                            ]
-                        },
-                        language: 'ko',
-                        image: {
-                            toolbar: [
-                                'imageTextAlternative',
-                                'imageStyle:full',
-                                'imageStyle:side'
-                            ]
-                        },
-                        table: {
-                            contentToolbar: [
-                                'tableColumn',
-                                'tableRow',
-                                'mergeTableCells'
-                            ]
-                        },
-                }
-            };
-        }
+if (process.client) {
+  CustomBuild = require('~/modules/ckeditor5-build-custom/build/ckeditor')
+  CKEditor = require('@ckeditor/ckeditor5-vue2')
+} else {
+  CKEditor = { component: { template: '<div></div>' } }
+}
+
+export default {
+    created(){
+
+    },
+    components: {
+        ckeditor: CKEditor.component
+    },
+    data() {
+        return {
+            editor: CustomBuild,
+            editorData: '<p>Content of the editor.</p>',
+            editorConfig: {
+                    toolbar: {
+                        items: [
+                            'heading',
+                            '|',
+                            'fontColor',
+                            'fontBackgroundColor',
+                            'bold',
+                            'italic',
+                            'link',
+                            'bulletedList',
+                            'numberedList',
+                            '|',
+                            'outdent',
+                            'indent',
+                            '|',
+                            'imageUpload',
+                            'blockQuote',
+                            'insertTable',
+                            'mediaEmbed',
+                            'undo',
+                            'redo'
+                        ]
+                    },
+                    language: 'ko',
+                    image: {
+                        toolbar: [
+                            'imageTextAlternative',
+                            'imageStyle:full',
+                            'imageStyle:side'
+                        ]
+                    },
+                    table: {
+                        contentToolbar: [
+                            'tableColumn',
+                            'tableRow',
+                            'mergeTableCells'
+                        ]
+                    },
+            }
+        };
     }
+}
 </script>
 
